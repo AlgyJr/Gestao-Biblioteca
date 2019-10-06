74ad864e09dc21388b135cc08d0f967c95c0a365
Algy Jr <algymussa@gmail.com>
2019-10-06 22:21:52 +0200

    Initial Commit

---
+++ Gestao-Biblioteca/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Gestao-Biblioteca
+//
+//  Created by ALgy Aly on 10/6/19.
+//  Copyright © 2019 ALgy Aly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
